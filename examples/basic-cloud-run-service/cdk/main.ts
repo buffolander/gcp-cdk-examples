cd16cf5ffe7e6ac5b5e3e49706c598ae7c0b4000
Bruno Soares <bruno.soares@tractable.ai>
2023-05-05 07:52:14 +0100

    Configure prettier

--- examples/basic-cloud-run-service/cdk/main.ts
+++ examples/basic-cloud-run-service/cdk/main.ts
@@ -1,33 +1,33 @@
-import { Container } from "@cdktf/provider-docker/lib/container";
-import { Image } from "@cdktf/provider-docker/lib/image";
-import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
-
+import { Container } from '@cdktf/provider-docker/lib/container'
+import { Image } from '@cdktf/provider-docker/lib/image'
+import { DockerProvider } from '@cdktf/provider-docker/lib/provider'
 import {
   GoogleProvider,
   GoogleProviderConfig,
-} from "@cdktf/provider-google/lib/provider";
+} from '@cdktf/provider-google/lib/provider'
 
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { nginxContainerConfig } from "./container";
+import { App, TerraformStack } from 'cdktf'
+import { Construct } from 'constructs'
+
+import { nginxContainerConfig } from './container'
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
-    super(scope, id);
+    super(scope, id)
 
-    const image = new Image(this, "nginx-image", {
-      name: "nginx:latest",
+    const image = new Image(this, 'nginx-image', {
+      name: 'nginx:latest',
       keepLocally: false,
-    });
+    })
 
     const providerConfig: GoogleProviderConfig = {
-      zone: "us-cental1-c",
-    };
-    new Container(this, "nginx-container", nginxContainerConfig(image));
-    new DockerProvider(this, "string");
+      zone: 'us-cental1-c',
+    }
+    new Container(this, 'nginx-container', nginxContainerConfig(image))
+    new DockerProvider(this, 'string')
   }
 }
 
-const app = new App();
-new MyStack(app, "basic-cloud-run-service");
-app.synth();
+const app = new App()
+new MyStack(app, 'basic-cloud-run-service')
+app.synth()
